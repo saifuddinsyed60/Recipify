6f046cbc6d62a5557e2fca90709b40ad3622b08d
Syed Saifuddin <166879130+saifuddinsyed60@users.noreply.github.com>
2024-11-19 01:07:30 -0600

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 const config = {
    // backendUrl: "http://127.0.0.1:5209",
-    backendUrl: "recipify-caced2b9g2eyb4cr.northcentralus-01.azurewebsites.net",
+    backendUrl: "https://recipify-caced2b9g2eyb4cr.northcentralus-01.azurewebsites.net",
   };
   
   export default config;
